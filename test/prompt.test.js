c7c4b6defda0cc543ef00f6d46f076b91f386ade
Dylan Greene <dylang@gmail.com>
2014-01-26 18:27:55 -0500

    bump version

--- test/prompt.test.js
+++ test/prompt.test.js
@@ -8,6 +8,7 @@ describe('grunt-prompt', function(){
 
         it('loads', function(){
             var prompt = require('../tasks/prompt');
+            console.log('--- The best way to test this is to just run `grunt` or `grunt prompt results`. ----')
             expect(prompt).to.exist;
         });
     });
